93600881222c1e145f88717b8b4e70f7f80bec0c
Sara Walker <sarawall1187@gmail.com>
2018-11-19 19:56:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board,index)
   if board[index] == [" "] || [""] || nil
    return false
-  #elsif board[index] == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-   #return true
+  elsif board[index] == ["X"] || ["O"]
+   return true
   end
 end
 
