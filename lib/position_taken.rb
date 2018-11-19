f7a7f89c5e1408658638042a568a871d87da461a
Sara Walker <sarawall1187@gmail.com>
2018-11-19 22:14:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,13 @@
 def position_taken?(board,index)
   if board[index] == [" "] || board[index] == [""] || board[index] == nil
- 
-   return false
- 
-  end
+    return false
+ end
 end
 
 
 #["", "", "", "", "", "", "", "", ""]
  #if board[index] == [" "] || [""] || nil
-
+ #elsif board[index] == [{"X"}] || [{"O"}]
+   #return true
 
 # code your #position_taken? method here!
