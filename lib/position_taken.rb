2766b828c338c060da0266f2df33428af48da2a8
Sara Walker <sarawall1187@gmail.com>
2018-11-19 21:12:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
-
-require "pry"
 def position_taken?(board,index)
   if board[index] == [" "] || [""] || nil
    return false
-  elsif binding.pry board[index] == ["X"] || ["O"]
+  elsif board[index] == ["X"] || ["O"]
    return true
   end
 end
